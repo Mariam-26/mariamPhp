920ea88ffad4e0fb6ab51908bca3ba4c6f1c213c
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-23 11:49:42 +0100

    php

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
-Coucou c'est moi !
-<?php
-$monPrenom = "Mariam";
-$monNom = "Ouédraogo";
-$note1 = 18;
-$note2 = 13.65;
-$moyenne = ($note1 + $note2) / 2;
-echo "$monPrenom $monNom vous avez eu $moyenne de moyenne";
-?>
 
-Je vous fais un coucou répondez moi !
+<?php
+// $eleve = ["Mariam", "Ouédraogo", [18, 13.65, 17.50, 16.50]];
+// echo $eleve[2][3];
+// $eleve = [
+//   "nom" => "Ouédraogo",
+//   "prenom" => "Mariam",
+//   "notes" => [18, 13.65, 17.50, 16.50]
+// ];
+// echo $eleve["prenom"] . "\n";
+// $eleve["prenom"] = "Olivia";
+// $eleve["notes"][1] = 17;
+// print_r($eleve["notes"]);
+
+// echo $eleve["prenom"] . " " . $eleve["nom"];
+$classe = [
+  [
+    "nom" => "Ouédraogo",
+    "prenom" => "Mariam",
+    "notes" => [18, 13.65, 17.50, 16.50]
+  ],
+  [
+    "nom" => "Darivon",
+    "prenom" => "Dominique",
+    "notes" => [19, 17, 17.50, 19.50]
+  ]
+];
+  
+echo $classe[1]["notes"][3];
+
