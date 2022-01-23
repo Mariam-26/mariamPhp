090cc16629c57749a20f3a82eb59bfbad5d08918
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-23 10:35:05 +0100

    php

---
+++ index.php
@@ -0,0 +1,11 @@
+Coucou c'est moi !
+<?php
+$monPrenom = "Mariam";
+$monNom = "Ouédraogo";
+$note1 = 18;
+$note2 = 13.65;
+$moyenne = ($note1 + $note2) / 2;
+echo "$monPrenom $monNom vous avez eu $moyenne de moyenne";
+?>
+
+Je vous fais un coucou répondez moi !
